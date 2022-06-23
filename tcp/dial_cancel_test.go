4400a60abef89108bd19cd1216a3000922d275af
Seongbeom27 <lgh121546@gmail.com>
2022-06-23 23:07:53 +0900

    Deadline Context를 사용하여 타임아웃 하기 Context를 취소하여 연결 중단

---
+++ tcp/dial_cancel_test.go
@@ -0,0 +1,47 @@
+package tcp
+
+import (
+	"context"
+	"net"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func TestDialContextCancel(t *testing.T) {
+	// context.WithCancel 함수를 이용하여 context와 context를 취소할 수 있는 함수를 받음
+	ctx, cancel := context.WithCancel(context.Background())
+	sync := make(chan struct{})
+
+	// 수동으로 context를 취소하기 때문에 closure를 만들어서 별도로 연결 시도를 처리 하기 위한 고루틴 시작
+	go func() {
+		defer func() { sync <- struct{}{} }()
+
+		var d net.Dialer
+		d.Control = func(_, _ string, _ syscall.RawConn) error {
+			time.Sleep(time.Second)
+			return nil
+		}
+
+		conn, err := d.DialContext(ctx, "tcp", "10.0.0.1:80")
+		if err != nil {
+			t.Log(err)
+			return
+		}
+
+		conn.Close()
+		t.Error("connection did not time out")
+	}()
+
+	// dialer가 연결 시도를 하고 원격 노드의 handshake가 끝나면,
+	// context를 취소하기 위해 cancel 함수를 호출
+	cancel()
+	<-sync
+
+	// 결과 : DialContext 메서드는 즉시 nil이 아닌 에러를 반환하고 고루틴을 종료
+
+	// Err 메서드는 context.Canceled를 반환
+	if ctx.Err() != context.Canceled {
+		t.Errorf("expected canceled context; actual: %q", ctx.Err())
+	}
+}
--- tcp/dial_context_test.go
+++ tcp/dial_context_test.go
@@ -9,17 +9,25 @@ import (
 )
 
 func TestDialContext(t *testing.T) {
+	// 5초 후 데드라인이 지나는 콘텍스트를 만들기 위해 현재 시간으로부터 5초 뒤의 시간을 저장
 	dl := time.Now().Add(5 * time.Second)
+
+	// WithDeadline 함수를 이용하여 context와 cancel 함수를 생성하고 위에서 생성한 데드라인을 설정
 	ctx, cancel := context.WithDeadline(context.Background(), dl)
+
+	// context가 가비지컬렉션이 되도록 cancel 함수를 defer로 호출
 	defer cancel()
 
 	var d net.Dialer // DialContext는 Dialer의 메서드
+
+	// Dialer의 Control 함수를 Overriding하여 연결을 context의 데드라인을 간신히 초과 (tijme.Milisecond)하는 정도로 지연
 	d.Control = func(_, _ string, _ syscall.RawConn) error {
 		// context의 데드라인이 지나기 위해 충분히 긴 시간 동안 대기
 		time.Sleep(5*time.Second + time.Millisecond)
 		return nil
 	}
 
+	// DialContext 함수의 첫 번째 매개변수로 위에서 생성한 context(데드라인 5초를 가지고 있는)를 전달
 	conn, err := d.DialContext(ctx, "tcp", "10.0.0.0:80")
 	if err == nil {
 		conn.Close()
@@ -35,6 +43,8 @@ func TestDialContext(t *testing.T) {
 		}
 	}
 
+	// 테스트의 끝 부분의 에러 처리는 데드라인이 context를 제대로 취소하였는지
+	// cancel 함수 호출에 문제는 없었는지 확인
 	if ctx.Err() != context.DeadlineExceeded {
 		t.Errorf("expected deadline exceeded; actual: %v", ctx.Err())
 	}
